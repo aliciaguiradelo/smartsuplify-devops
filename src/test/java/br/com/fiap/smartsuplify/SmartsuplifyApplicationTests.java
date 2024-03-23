d977cb1ef27bcf29913b3e9d3f7e1f65235fe397
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-03-22 22:48:31 -0300

    add anottation EnableJpaRepositories na classe de test

--- src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
+++ src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.fiap.smartsuplify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@EnableJpaRepositories
 class SmartsuplifyApplicationTests {
 
 	@Test
