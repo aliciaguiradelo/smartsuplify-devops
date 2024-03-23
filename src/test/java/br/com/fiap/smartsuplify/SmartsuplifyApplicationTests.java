e1da50d6e6bbfa65cd64270f1ae2ca17ad124bf3
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-03-22 23:10:33 -0300

    subistituindo o Enable pelo Dirties

--- src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
+++ src/test/java/br/com/fiap/smartsuplify/SmartsuplifyApplicationTests.java
@@ -2,10 +2,10 @@ package br.com.fiap.smartsuplify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
-@EnableJpaRepositories
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class SmartsuplifyApplicationTests {
 
 	@Test
